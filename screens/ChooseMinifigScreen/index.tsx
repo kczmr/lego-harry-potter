b723842cba77a7c0a0df412ccfa7ef84889ff069
Andrzej Kaczmarek <ndrzejkaczmarek@gmail.com>
2023-03-14 23:02:00 +0100

    feat: navigation setup

---
+++ screens/ChooseMinifigScreen/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ChooseMinifigScreen: React.FC = () => (
+  <View>
+    <Text>Choose minifig screen</Text>
+  </View>
+);
+
+export default ChooseMinifigScreen;
---
+++ setup/Navigation/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import ChooseMinifigScreen from '../../screens/ChooseMinifigScreen';
+import { MainStackParamList } from './types';
+
+const { Navigator, Screen } = createStackNavigator<MainStackParamList>();
+
+const Navigation: React.FC = () => (
+  <Navigator>
+    <Screen name='ChooseMinifigScreen' component={ChooseMinifigScreen} />
+  </Navigator>
+);
+
+export default Navigation;
